fbb10ddac9086b43b7a437bd260e9f920f8ffc20
Emma Beldick <emma.beldick@mail.concordia.ca>
2026-09-24 14:49:02 -0400

    modified the ball teleporting when it hits the court

--- P1_Pong/Assets/Scripts/Court.cs
+++ P1_Pong/Assets/Scripts/Court.cs
@@ -2,19 +2,26 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+
 public class Court : MonoBehaviour
 {
     public GameManager gameManager;
     public int courtId = 0;
 
+
     public EventTrigger.TriggerEvent courtTrigger;
 
+// where the ball will teleport
+    public Transform TeleportPoint;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Ball ball = collision.gameObject.GetComponent<Ball>();
         if (ball == null) return;
 
+    // teleport the ball
+        ball.transform.position = TeleportPoint.position;
+
         BaseEventData eventData = new BaseEventData(EventSystem.current);
-        courtTrigger.Invoke(eventData);
+       
     }
 }
